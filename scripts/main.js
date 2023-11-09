52b29b45632d5081df7910d3fe080ae2f9ad01c2
aldm <d44.a1@yandex.ru>
2023-11-09 17:31:16 +0300

    ss

--- scripts/main.js
+++ scripts/main.js
@@ -13,3 +13,29 @@ const menuDrop = document.querySelector(".header__menu");
 dropButton.addEventListener("click", () => {
   menuDrop.classList.toggle("active");
 });
+
+const tabItems = Array.from(
+  document.querySelectorAll(".catalog__select-title")
+);
+const formItems = Array.from(
+  document.querySelectorAll(".catalog__select-item")
+);
+
+const clearActiveTabs = (element, className = "active") => {
+  element.find((item) => item.classList.remove(`${className}`));
+};
+
+const setActiveTabs = (element, index, className = "active") => {
+  element[index].classList.add(`${className}`);
+};
+
+const chekTab = (item, index) => {
+  item.addEventListener("click", () => {
+    clearActiveTabs(tabItems);
+    clearActiveTabs(formItems);
+
+    setActiveTabs(tabItems, index);
+    setActiveTabs(formItems, index);
+  });
+};
+tabItems.forEach(chekTab);
