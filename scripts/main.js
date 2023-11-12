3f7441921ad7be7b72db05bca518053495fd9e03
aldm <d44.a1@yandex.ru>
2023-11-12 16:46:42 +0300

    w

--- scripts/main.js
+++ scripts/main.js
@@ -16,6 +16,7 @@ dropButton.addEventListener("click", () => {
   menuDrop.classList.toggle("active");
   menuArrow.classList.toggle("active");
   menuLinkColor.classList.toggle("active");
+  menuSearch.classList.remove("active");
 });
 
 const tabItems = Array.from(
@@ -43,3 +44,20 @@ const chekTab = (item, index) => {
   });
 };
 tabItems.forEach(chekTab);
+
+/***********search***********/
+const buttonSearch = document.querySelector(".button-search");
+const menuSearch = document.querySelector(".search__menu");
+
+buttonSearch.addEventListener("click", () => {
+  menuSearch.classList.toggle("active");
+  menuDrop.classList.remove("active");
+  resultSearch.classList.remove("active");
+});
+
+const resulBtnSearch = document.querySelector(".button-search-menu");
+const resultSearch = document.querySelector(".search__result");
+
+resulBtnSearch.addEventListener("click", () => {
+  resultSearch.classList.add("active");
+});
