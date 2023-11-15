4c2c1f8690bdb38df38b95d7a3febd446c6b207b
aldm <d44.a1@yandex.ru>
2023-11-16 00:49:49 +0300

    wda

--- scripts/main.js
+++ scripts/main.js
@@ -1,89 +1,66 @@
-// window.onscroll = function showHeader() {
-//   let header = document.querySelector(".navigation");
+// const dropButton = document.getElementById("menu-drop");
+// const menuDrop = document.querySelector(".header__menu");
+// const menuArrow = document.querySelector(".menu-drop");
+// const menuLinkColor = document.querySelector(".menu-item");
 
-//   if (window.pageYOffset > 1080) {
-//     header.classList.add("header__fixed");
-//   } else {
-//     header.classList.remove("header__fixed");
-//   }
-// };
-const dropButton = document.getElementById("menu-drop");
-const menuDrop = document.querySelector(".header__menu");
-const menuArrow = document.querySelector(".menu-drop");
-const menuLinkColor = document.querySelector(".menu-item");
+// const navOuth = document.querySelector(".navigation");
 
-// dropButton.addEventListener("mouseover", () => {
-//   menuDrop.classList.toggle("active");
-//   menuArrow.classList.toggle("active");
-//   menuLinkColor.classList.toggle("active");
-//   menuSearch.classList.remove("active");
-//   pageLock.classList.toggle("lock");
+// navOuth.addEventListener("click", () => {
+//   menuDrop.classList.remove("active");
 // });
 
-const navOuth = document.querySelector(".navigation");
+// const tabItems = Array.from(document.querySelectorAll(".catalog-hover"));
+// const formItems = Array.from(
+//   document.querySelectorAll(".catalog__select-item")
+// );
 
-navOuth.addEventListener("click", () => {
-  menuDrop.classList.remove("active");
-  pageLock.classList.remove("lock");
-});
+// const clearActiveTabs = (element, className = "active") => {
+//   element.find((item) => item.classList.remove(`${className}`));
+// };
 
-const tabItems = Array.from(
-  document.querySelectorAll(".catalog__select-title")
-);
-const formItems = Array.from(
-  document.querySelectorAll(".catalog__select-item")
-);
+// const setActiveTabs = (element, index, className = "active") => {
+//   element[index].classList.add(`${className}`);
+// };
 
-const clearActiveTabs = (element, className = "active") => {
-  element.find((item) => item.classList.remove(`${className}`));
-};
+// const chekTab = (item, index) => {
+//   item.addEventListener("click", () => {
+//     clearActiveTabs(tabItems);
+//     clearActiveTabs(formItems);
 
-const setActiveTabs = (element, index, className = "active") => {
-  element[index].classList.add(`${className}`);
-};
-
-const chekTab = (item, index) => {
-  item.addEventListener("click", () => {
-    clearActiveTabs(tabItems);
-    clearActiveTabs(formItems);
-
-    setActiveTabs(tabItems, index);
-    setActiveTabs(formItems, index);
-  });
-};
-tabItems.forEach(chekTab);
+//     setActiveTabs(tabItems, index);
+//     setActiveTabs(formItems, index);
+//   });
+// };
+// tabItems.forEach(chekTab);
 
 /***********search***********/
-const buttonSearch = document.querySelector(".button-search");
-const menuSearch = document.querySelector(".search__menu");
+// const buttonSearch = document.querySelector(".button-search");
+// const menuSearch = document.querySelector(".search__menu");
 
-buttonSearch.addEventListener("click", () => {
-  menuSearch.classList.toggle("active");
-  menuDrop.classList.remove("active");
-  resultSearch.classList.remove("active");
-});
+// buttonSearch.addEventListener("click", () => {
+//   menuSearch.classList.toggle("active");
+//   menuDrop.classList.remove("active");
+//   resultSearch.classList.remove("active");
+// });
 
-const resulBtnSearch = document.querySelector(".button-search-menu");
-const resultSearch = document.querySelector(".search__result");
+// const resulBtnSearch = document.querySelector(".button-search-menu");
+// const resultSearch = document.querySelector(".search__result");
 
-resulBtnSearch.addEventListener("click", () => {
-  resultSearch.classList.add("active");
-});
+// resulBtnSearch.addEventListener("click", () => {
+//   resultSearch.classList.add("active");
+// });
 
 /***burger***/
-const burgerOpen = document.querySelector(".burger");
+const burgerOpen = document.getElementById("burgerOpen");
 const menuTab = document.querySelector(".mobile-menu");
-const pageLock = document.querySelector(".page");
 const burgerClose = document.querySelector(".mobile-menu__burger");
 
 burgerOpen.addEventListener("click", () => {
   menuTab.classList.add("active");
-  pageLock.classList.add("lock");
 });
 
 burgerClose.addEventListener("click", () => {
   menuTab.classList.remove("active");
-  pageLock.classList.remove("lock");
 });
 
 const catButton = document.querySelector(".mobile-menu__catalog");
