5c7fdcdf8c7bab84f093328259fbe8c5cabd1743
aldm <d44.a1@yandex.ru>
2023-11-18 19:32:12 +0300

    вцф

--- scripts/swiper.js
+++ scripts/swiper.js
@@ -1,6 +1,43 @@
-const brands_slider = new Swiper(".brands_slider", {
-  slidesPerView: 5,
-  spaceBetween: 44,
+// const brands_slider = new Swiper(".brands_slider", {
+//   slidesPerView: 5,
+//   spaceBetween: 44,
+//   speed: 2500,
+//   loop: true,
+//   allowTouchMove: false,
+//   autoplay: {
+//     delay: 0,
+//     enabled: true,
+//     disableOnInteraction: false,
+//   },
+//   breakpoints: {
+//     0: {
+//       slidesPerView: 3,
+//       spaceBetween: 30,
+//     },
+//     500: {
+//       slidesPerView: 4,
+//       spaceBetween: 44,
+//     },
+//     768: {
+//       slidesPerView: 4,
+//       spaceBetween: 44,
+//     },
+//     800: {
+//       slidesPerView: 3,
+//       spaceBetween: 44,
+//     },
+//     1200: {
+//       slidesPerView: 4,
+//     },
+//     1520: {
+//       slidesPerView: 5,
+//     },
+//   },
+// });
+/******/
+const contact_slider = new Swiper(".contact_slider", {
+  slidesPerView: 1,
+  spaceBetween: 5,
   speed: 2500,
   loop: true,
   allowTouchMove: false,
@@ -9,28 +46,28 @@ const brands_slider = new Swiper(".brands_slider", {
     enabled: true,
     disableOnInteraction: false,
   },
-  breakpoints: {
-    0: {
-      slidesPerView: 3,
-      spaceBetween: 30,
-    },
-    500: {
-      slidesPerView: 4,
-      spaceBetween: 44,
-    },
-    768: {
-      slidesPerView: 4,
-      spaceBetween: 44,
-    },
-    800: {
-      slidesPerView: 3,
-      spaceBetween: 44,
-    },
-    1200: {
-      slidesPerView: 4,
-    },
-    1520: {
-      slidesPerView: 5,
-    },
-  },
+  // breakpoints: {
+  //   0: {
+  //     slidesPerView: 3,
+  //     spaceBetween: 30,
+  //   },
+  //   500: {
+  //     slidesPerView: 4,
+  //     spaceBetween: 44,
+  //   },
+  //   768: {
+  //     slidesPerView: 4,
+  //     spaceBetween: 44,
+  //   },
+  //   800: {
+  //     slidesPerView: 3,
+  //     spaceBetween: 44,
+  //   },
+  //   1200: {
+  //     slidesPerView: 4,
+  //   },
+  //   1520: {
+  //     slidesPerView: 5,
+  //   },
+  // },
 });
