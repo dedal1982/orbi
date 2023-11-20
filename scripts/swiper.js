f31666444dfcd2087ceba5403345b3d9d92b8351
aldm <d44.a1@yandex.ru>
2023-11-20 15:01:49 +0300

    вц

--- scripts/swiper.js
+++ scripts/swiper.js
@@ -34,11 +34,12 @@ const brands_slider = new Swiper(".brands_slider", {
     },
   },
 });
-/******/
+
+/***contact_slider***/
 const contact_slider = new Swiper(".contact_slider", {
   slidesPerView: 1,
   spaceBetween: 10,
-  speed: 2500,
+  speed: 5000,
   loop: true,
   allowTouchMove: false,
   autoplay: {
@@ -46,28 +47,4 @@ const contact_slider = new Swiper(".contact_slider", {
     enabled: true,
     disableOnInteraction: false,
   },
-  // breakpoints: {
-  //   0: {
-  //     slidesPerView: 3,
-  //     spaceBetween: 30,
-  //   },
-  //   500: {
-  //     slidesPerView: 4,
-  //     spaceBetween: 44,
-  //   },
-  //   768: {
-  //     slidesPerView: 4,
-  //     spaceBetween: 44,
-  //   },
-  //   800: {
-  //     slidesPerView: 3,
-  //     spaceBetween: 44,
-  //   },
-  //   1200: {
-  //     slidesPerView: 4,
-  //   },
-  //   1520: {
-  //     slidesPerView: 5,
-  //   },
-  // },
 });
